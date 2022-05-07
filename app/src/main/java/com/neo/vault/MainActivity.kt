cfe984f1425bc3104fd447a95cdc10165e77a818
irineu silva <irineu.silva@aiqfome.com>
2022-05-06 23:27:34 -0300

    refactor: enable view binding in app module

--- app/src/main/java/com/neo/vault/MainActivity.kt
+++ app/src/main/java/com/neo/vault/MainActivity.kt
@@ -1,11 +1,17 @@
 package com.neo.vault
 
-import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import androidx.appcompat.app.AppCompatActivity
+import com.neo.vault.databinding.ActivityMainBinding
 
 class MainActivity : AppCompatActivity() {
+
+    lateinit var binding: ActivityMainBinding
+
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
-        setContentView(R.layout.activity_main)
+
+        binding = ActivityMainBinding.inflate(layoutInflater)
+        setContentView(binding.root)
     }
 }
